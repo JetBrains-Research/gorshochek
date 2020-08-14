6455f7dbea696a28f01837163174e55ec81d83da
maximzubkov <zubkov.md@phystech.edu>
2020-08-14 15:45:18 +0300

    Add install_requirements and fix workflow

---
+++ scripts/install_requirements.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Run script from code2seq dir using command:
+#    sh scripts/install_requirements.sh
+
+BUILD_DIR=./build
+
+OS=$(uname -s)
+case "$OS" in
+  Darwin*)
+    echo "OSX"
+    DISTR_VERSION="darwin-apple"
+    ;;
+  Linux*)
+    echo "LINUX"
+    DISTR=$(lsb_release -is | tr "[:upper:]" "[:lower:]")
+    VERSION=$(lsb_release -sr | tr "[:upper:]" "[:lower:]")
+    DISTR_VERSION="linux-gnu-${DISTR}-${VERSION}"
+    ;;
+  *)
+    echo "unknown: $OS"
+    ;;
+esac
+
+if [ ! -d "$BUILD_DIR" ]
+then
+  mkdir "$BUILD_DIR"
+fi
+
+DISTR_ARCHIVE="clang+llvm-9.0.0-x86_64-${DISTR_VERSION}.tar.xz"
+DISTR_LINK="https://releases.llvm.org/9.0.0/${DISTR_ARCHIVE}"
+if [ ! -f "$BUILD_DIR"/"$DISTR_ARCHIVE" ]
+then
+  wget "$DISTR_LINK" -P "$BUILD_DIR"
+fi
+
+tar -xvf "$BUILD_DIR"/"$DISTR_ARCHIVE" -C "$BUILD_DIR"
+mv "$BUILD_DIR"/"clang+llvm-9.0.0-x86_64-${DISTR_VERSION}" "$BUILD_DIR"/"clang+llvm-9"
+
