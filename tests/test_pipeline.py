c431e282bc4d500285de4cf606db121baa5c6376
maximzubkov <zubkov.md@phystech.edu>
2021-01-15 14:24:14 +0300

    Refactor tests and update logging tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,7 +7,7 @@ import pytest
 
 configs_dir_path = path.join("tests", "configs")
 logs_file = path.join("tests", "clang_logs.txt")
-expected_path = path.join("tests", "resources", "expected")
+default_expected_path = path.join("tests", "resources", "expected")
 actual_path = path.join("tests", "resources", "actual")
 input_path = path.join("tests", "resources", "input")
 clang_path = path.join("c++")
@@ -15,7 +15,12 @@ tricky_path = path.join("scripts", "..", "tests", "..", "tests", "resources", "i
 build = path.join("build", "gorshochek")
 
 
-def _test(input_: str, config_path: str) -> None:
+def _test(
+        input_: str,
+        config_path: str,
+        expected_path: str = default_expected_path,
+        check_compilation: bool = True
+) -> None:
     # Getting the caller function name
     test_name = inspect.stack()[1].function
     full_input_path = path.join(input_path, input_)
@@ -38,31 +43,41 @@ def _test(input_: str, config_path: str) -> None:
         for i in range(expected_num_transforms - 1):
             expected_transform_path = path.join(expected_file_dir, f"transformation_{i}.cpp")
             actual_transform_path = path.join(actual_file_dir, f"transformation_{i}.cpp")
-            try:
-                subprocess.check_output([clang_path, actual_transform_path, "-o",
-                                         path.join(actual_path, f"{i}.o")],
-                                        universal_newlines=True,
-                                        stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as e:
-                with open(logs_file, "a+") as logs:
-                    output = "\n\t".join(e.output.split("\n")[:-1])
-                    logs.write(f"- Test: {test_name}\n" +
-                               f"  File: {file_name}\n" +
-                               f"  Transformation: transformation_{i}.cpp:\n\t" + output + "\n")
-                pytest.fail(f"During test {test_name} compilation of {test_name}/transformation_{i}.cpp\n" +
-                            "exited with non-zero code, for more details go to tests/clang_log.txt")
+            if check_compilation:
+                try:
+                    subprocess.check_output([clang_path, actual_transform_path, "-o",
+                                             path.join(actual_path, f"{i}.o")],
+                                            universal_newlines=True,
+                                            stderr=subprocess.STDOUT)
+                except subprocess.CalledProcessError as e:
+                    with open(logs_file, "a+") as logs:
+                        output = "\n\t".join(e.output.split("\n")[:-1])
+                        logs.write(f"- Test: {test_name}\n" +
+                                   f"  File: {file_name}\n" +
+                                   f"  Transformation: transformation_{i}.cpp:\n\t" + output + "\n")
+                    pytest.fail(f"During test {test_name} compilation of {test_name}/transformation_{i}.cpp\n" +
+                                "exited with non-zero code, for more details go to tests/clang_log.txt")
             with open(expected_transform_path, "r") as expected:
                 expected_data = expected.read()
             with open(actual_transform_path, "r") as transformed:
                 transformed_data = transformed.read()
-            assert transformed_data == expected_data, "Actual and expected files mismatch "
+            assert transformed_data == expected_data, "Actual and expected files mismatch"
+    expected_log_path = path.join(expected_path, "log.txt")
+    actual_log_path = path.join(actual_path, "log.txt")
+    print(actual_log_path, expected_log_path, path.exists(actual_log_path), path.exists(expected_log_path))
+    if path.exists(actual_log_path) and path.exists(expected_log_path):
+        with open(expected_log_path) as exp:
+            expected_log = exp.read()
+        with open(actual_log_path) as act:
+            actual_log = act.read()
+        assert expected_log == actual_log
     shutil.rmtree(actual_path)
 
 
 def test_pipeline() -> None:
     config_path = path.join(configs_dir_path, "test_pipeline_config.yaml")
     input_ = "test_pipeline"
-    _test(input_, config_path)
+    _test(input_, config_path, expected_path=path.join(default_expected_path, input_))
 
 
 def test_remove_comments() -> None:
@@ -122,4 +137,4 @@ def test_open_macros() -> None:
 def test_logging() -> None:
     config_path = path.join(configs_dir_path, "test_logging_config.yaml")
     input_ = "test_logging"
-    _test(input_, config_path)
+    _test(input_, config_path, expected_path=path.join(default_expected_path, input_), check_compilation=False)
