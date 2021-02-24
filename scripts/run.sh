6cca9571f1880dabbc9cc745d8e71eb5abbf37a7
maximzubkov <zubkov.md@phystech.edu>
2021-02-24 23:27:56 +0300

    Add processing logs

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@
 # $2              specify a path to dir where transformed files are going to be stored
 # $3              [optional] specify a path to config file
 
-BUFFER_SIZE=10
+BUFFER_SIZE=100
 IN_BUFFER=in_buffer
 if [ ! -d "$IN_BUFFER" ]
 then
@@ -18,6 +18,7 @@ if [ ! -d "$OUT_BUFFER" ]
 then
   mkdir -p "$OUT_BUFFER"
 fi
+LOG_BUFFER=log_buffer.txt
 
 INPUT_PATH=$1
 OUTPUT_PATH=$2
@@ -33,41 +34,47 @@ else
   CONFIG_PATH=config.yaml
 fi
 
+transform () {
+  docker run --ipc=host --uts=host \
+                -v $PWD/"${IN_BUFFER}":/gorshochek/data/input \
+                -v $PWD/"${OUT_BUFFER}":/gorshochek/data/output \
+                -i -t gorshochek "${CONFIG_PATH}"
+  logs=$(find "${OUT_BUFFER}"/* -name "*.txt")
+  for log in $logs
+  do
+    "$log" > "$LOG_BUFFER"
+  done
+  mv "$OUT_BUFFER"/* "$OUTPUT_PATH"
+}
+
 index=0;
 files=$(find "${INPUT_PATH}"/* -type f)
 for file in $files
 do
         echo "$file"
-        sudo cp "$file" "$IN_BUFFER"
+        cp "$file" "$IN_BUFFER"
         if ((index % BUFFER_SIZE == BUFFER_SIZE - 1))
         then
           echo "$OUT_BUFFER"
-          sudo docker run --ipc=host --uts=host \
-                -v $PWD/"${IN_BUFFER}":/gorshochek/data/input \
-                -v $PWD/"${OUT_BUFFER}":/gorshochek/data/output \
-                -i -t gorshochek "${CONFIG_PATH}"
-          mv "$OUT_BUFFER"/* "$OUTPUT_PATH"
+          transform
           find "$IN_BUFFER" -type f -delete
         fi
         index=$((index+1))
 done
 
-sudo docker run --ipc=host --uts=host \
-      -v $PWD/"${IN_BUFFER}":/gorshochek/data/input \
-      -v $PWD/"${OUT_BUFFER}":/gorshochek/data/output \
-      -i -t gorshochek "${CONFIG_PATH}"
-cp -r "$OUT_BUFFER"/* "$OUTPUT_PATH"
+transform
+mv "$LOG_BUFFER" "$OUTPUT_PATH"
 rm -rf "$IN_BUFFER"
 rm -rf "$OUT_BUFFER"
 
-#files=$(find "$OUTPUT_PATH"/* -type d)
-#for file in $files;
-#  do
-#    fdupes -dN -r "$file"
-#    transformed=$(find "$file" -type f -name "*.cpp")
-#    if [[ -z $transformed ]]
-#    then
-#      echo "remove all"
-#      rm -rf "$file"
-#    fi
-#  done
+files=$(find "$OUTPUT_PATH"/* -type d)
+for file in $files;
+  do
+    fdupes -dN -r "$file"
+    transformed=$(find "$file" -type f -name "*.cpp")
+    if [[ -z $transformed ]]
+    then
+      echo "remove all"
+      rm -rf "$file"
+    fi
+  done
