14d1733725a22788da7a913eba0773388ea20454
maximzubkov <zubkov.md@phystech.edu>
2021-04-16 14:11:23 +0300

    Postprocessing tests

---
+++ tests/test_postprocessing.py
@@ -0,0 +1,33 @@
+import zipfile
+import shutil
+import subprocess
+from os import path, listdir, remove, walk
+
+import pytest
+
+data_path = "data"
+cf_reduced_data_path = path.join(data_path, "codeforces_reduced")
+script_path = path.join("scripts", "postprocessing", "postprocessing.sh")
+fdupes_script = ["fdupes", "-r", cf_reduced_data_path]
+
+
+def test_postprocessing() -> None:
+    try:
+        subprocess.check_call(["sh", script_path])
+    except subprocess.CalledProcessError:
+        pytest.fail(f"Running postprocessing script ended with non-zero code")
+    assert path.exists(data_path), f"Data path does not exists"
+    assert path.exists(cf_reduced_data_path), f"Codeforces reduced does not exists"
+
+    try:
+        output = subprocess.check_output(" ".join(fdupes_script), shell=True)
+        assert not output.strip()
+    except subprocess.CalledProcessError:
+        pytest.fail(f"Running fdupes script ended with non-zero code")
+
+    for file in listdir(cf_reduced_data_path):
+        if file not in ["duplicates.txt", "clones.txt"]:
+            assert path.isdir(path.join(cf_reduced_data_path, file))
+
+    files_number = sum(len(files) for root, dirs, files in walk(cf_reduced_data_path))
+    assert files_number
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,7 +9,7 @@ script_path = path.join("scripts", "codeforces", "download_data.sh")
 data_path = "data"
 cf_zip_path = path.join(data_path, "codeforces.zip")
 cf_data_path = path.join(data_path, "codeforces")
-duplicates_file_path = path.join(cf_data_path, "duplicates.txt")
+duplicates_file_path = path.join(cf_data_path, "clones.txt")
 tmp_data_path = path.join(data_path, "anti-plagiarism-datasets-master")
 rounds_storage_path = path.join(tmp_data_path, "rounds")
 
@@ -22,7 +22,7 @@ def test_downloading() -> None:
     assert path.exists(data_path), f"Data path does not exists"
     assert path.exists(cf_zip_path), f"Codeforces zip was not downloaded"
     assert path.exists(cf_data_path), f"Codeforces was not unzipped"
-    assert path.exists(duplicates_file_path), f"Codeforces duplicates does not exists"
+    assert path.exists(duplicates_file_path), f"Codeforces clones does not exists"
 
     with zipfile.ZipFile(cf_zip_path, "r") as zip_:
         zip_.extractall(data_path)
@@ -42,15 +42,5 @@ def test_downloading() -> None:
             for file in listdir(task_path):
                 ext = file.rsplit(".", 1)[1]
                 assert ext in ["cpp", "c"]
-
-    with open(duplicates_file_path, "r") as f:
-        duplicates_text = f.read()
-
-    for duplicates_batch in duplicates_text.split("\n\n"):
-        rows = duplicates_batch.split("\n")
-        for file_path in rows:
-            assert path.exists(file_path)
-            ext = file_path.rsplit(".", 1)[1]
-            assert ext in ["cpp", "c"]
 
     shutil.rmtree(data_path)
